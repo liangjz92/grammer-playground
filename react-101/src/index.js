06547272f7099d3bd9dd3291f14104bdd0ba0140
梁建增 <jiangzeng.ljz@alibaba-inc.com>
2019-02-01 14:09:23 +0800

    React 官网例子30%

--- react-101/src/index.js
+++ react-101/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ShoppingList from './pages/ShoppingList';
+import Game from './pages/Game';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<ShoppingList />, document.getElementById('test'));
+ReactDOM.render(<Game />, document.getElementById('test'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
